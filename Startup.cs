65e4623e9982f0099cb9eec2a77954f75c935169
Mohamed Romdhani <mromdhani7@gmail.com>
2018-12-28 10:18:49 +0100

    Cors added

--- Startup.cs
+++ Startup.cs
@@ -27,6 +27,7 @@ namespace MonProjetBanking_Back
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddDbContext<ComptesContext>(opt =>
                  opt.UseInMemoryDatabase("ComptesList"));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
@@ -44,6 +45,9 @@ namespace MonProjetBanking_Back
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+        app.UseCors(builder =>
+           builder.WithOrigins("http://localhost:4201"));
 
             //app.UseHttpsRedirection();
             app.UseMvc();
